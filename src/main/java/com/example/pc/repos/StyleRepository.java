a71d82b65b4cf733b5c2bf861e64a2d4efb7399e
ErenYea9er69 <rayenfootball.24@gmail.com>
2026-03-11 16:43:14 +0100

    delete

--- src/main/java/com/example/pc/repos/StyleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pc.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.pc.entities.Style;
-
-public interface StyleRepository extends JpaRepository<Style, Long> {
-}
